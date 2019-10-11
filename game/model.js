7ce5e33074964d7a587d51fcf9ba7faf81a2e84e
krisi <kokobluewinkoko@gmail.com>
2019-10-11 09:39:36 +0200

    Finaliza lobby and implement attack, need to fix stream in game router

--- game/model.js
+++ game/model.js
@@ -1,26 +1,28 @@
-const Sequelize=require('sequelize')
-const sequelize=require('../db')
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
 const User = require('../user/model')
 
 const Game = sequelize.define('game', {
-  name: {
-    type: Sequelize.STRING,
-    unique:true
-  },
-  healthp1:{
-   type: Sequelize.INTEGER
-  },
-  healthp2:{
-    type: Sequelize.INTEGER
-   },
-   turn: {
-    type: Sequelize.STRING
-   },
-   winner: {
-     type: Sequelize.STRING
-   }
-}) 
+    firstPlayer: {
+        type: Sequelize.INTEGER
+    },
+    secondPlayer: {
+        type: Sequelize.INTEGER
+    },
+    healthp1: {
+        type: Sequelize.INTEGER
+    },
+    healthp2: {
+        type: Sequelize.INTEGER
+    },
+    turn: {
+        type: Sequelize.STRING
+    },
+    winner: {
+        type: Sequelize.STRING
+    }
+})
 // User.belongsTo(Game)
 Game.hasMany(User)
- 
+
 module.exports = Game
--- game/router.js
+++ game/router.js
@@ -1,4 +1,4 @@
-const { Router } = require('express')
+const {Router} = require('express')
 const Game = require('./model')
 const Sse = require('json-sse')
 const User = require('../user/model')
@@ -7,44 +7,62 @@ const stream = new Sse()
 const router = new Router()
 
 router.get('/game', //auth,
-  async (req, res, next) => {
-    const game = await Game.findAll({
-      include: [
-        { model: User}
-      ]
+    async (req, res, next) => {
+        const game = await Game.findAll({
+            include: [
+                {model: User}
+            ]
+        })
+        const data = JSON.stringify(game)
+        stream.updateInit(data)
+        stream.init(req, res)
     })
-    const data = JSON.stringify(game)
-    res.send(game)
-    stream.updateInit(data)
-    stream.init(req, res)
-  })
 
 router.post('/game',
-  async (req, res, next) => {
-    const game1 = {
-      name: req.body.name,
-      healthp1: 200,
-      healthp2: 200,
-      turn: req.body.turn,
-      winner: req.body.winner
-    }
-    await Game.create(game1)
-    const game = await Game.findAll()
-    const data = JSON.stringify(game)
-    stream.send(data)
-    res.status(200)
-    res.send('Game created')
-  })
+    async (req, res, next) => {
+        const game1 = {
+            name: req.body.name,
+            healthp1: 200,
+            healthp2: 200,
+            turn: req.body.turn,
+            winner: req.body.winner
+        }
+        await Game.create(game1)
+        const game = await Game.findAll()
+        const data = JSON.stringify(game)
+        stream.send(data)
+        res.status(200)
+        res.send('Game created')
+    })
 
-router.put('/game/:id',
-  (req, res, next) => {
-    const id = req.params.id
-    Game.findByPk(id)
-      .then(game => {
-        game.update({healthp1: game.healthp1-20})
-      })
-      .then(game => res.send(game))
-      .catch(next)
-  })
+router.put('/game/fight',
+    async (req, res, next) => {
+        const id = req.body.gameId
+        const game = await Game.findByPk(id)
+        if (game) {
+            if (req.body.fightAction === "Attack") {
+                if (game.firstPlayer === parseInt(req.body.userId)) {
+                    const player1Health = game.healthp1;
+                    await Game.update({healthp1: player1Health - 20}, {where: {id: req.body.gameId}})
+                    const updated = await Game.findByPk(id)
+                    const data = JSON.stringify(updated)
+                    console.log('sending back game', data)
+                    res.status(200)
+                    stream.send({"data": data, type: 'GAME_PLAYER_ATACKED'})
+
+                } else if (game.secondPlayer === parseInt(req.body.userId)) {
+                    const player2Health = game.healthp2;
+                    await Game.update({healthp2: player2Health - 20}, {where: {id: req.body.gameId}})
+                    const updated = await Game.findByPk(id)
+                    const data = JSON.stringify(updated)
+                    console.log('sending back game', data)
+                    res.status(200)
+                    stream.send({"data": data, type: 'GAME_PLAYER_ATTACKED'})
+
+                }
+            }
+
+        }
+    })
 
 module.exports = router
